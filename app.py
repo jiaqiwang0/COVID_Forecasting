2148cf21f914f38cc409a5b64b45290dfbcf81cc
Ashley Shen <ashleyshen101@gmail.com>
2022-12-06 21:03:56 -0800

    add ui

--- app.py
+++ app.py
@@ -1,56 +1,77 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
 import pandas as pd
 import numpy as np
 import json
 import plotly
 from plotly.subplots import make_subplots
 from statsmodels.tsa.api import ExponentialSmoothing
-#import requests
+import requests
+import io
 
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    # get new data
-    train_dataset = get_new_data()
-    # fit model and predict
-    # need to specify which country/region, currentlu use the first country
-    train = train_dataset.values[:1][0]
+    return render_template('notdash.html')
+
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # default country = China
+    country_or_region = "Algeria"
+    if request.method == "POST":
+        result = request.form
+        country_or_region = result["country"]
+
+    train = get_new_data(country_or_region)
     fit = ExponentialSmoothing(train, seasonal_periods=3).fit()
     prediction = fit.forecast(30)
     # polt
     graphJSON = polt_scatter(train, prediction)
-    return render_template('notdash.html', graphJSON=graphJSON)
+
+    return render_template('notdash.html', graphJSON=graphJSON, cur_value=country_or_region)
 
 
-def get_new_data():
-    """
-    TODO get updated data
-    url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
-    raw_page = requests.get(url)
-    print(raw_page.text)
-    """
-    cofirmed_global_data = pd.read_csv("time_series_covid19_confirmed_global.csv")
-    drop_clo = ['Province/State','Country/Region','Lat','Long']
-    cofirmed_global_data_modified = cofirmed_global_data.drop(drop_clo,axis=1)
-    datewise= list(cofirmed_global_data_modified.columns)
-    # recent 100 days data as training data
-    train_dataset = cofirmed_global_data_modified[datewise[-100:]]
-    #val_dataset = train_dataset[datewise[-30:]]
-    return train_dataset
+def read_online_csv(url: str, country_or_region: str) -> pd.DataFrame:
+    content = requests.get(url).content
+    data = pd.read_csv(io.StringIO(content.decode("utf-8"))
+                       ).drop(["Province/State", "Lat", "Long"], axis=1)
+    return data[data["Country/Region"] == country_or_region].groupby("Country/Region").sum()
+
+
+def get_new_data(country_or_region: str) -> np.ndarray:
+    confirmed_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+    # deaths_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv"
+    # recovered_url = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv"
+
+    confirmed_data = read_online_csv(confirmed_url, country_or_region)
+    # deaths_data = read_online_csv(deaths_url, country_or_region)
+    # recovered_data = read_online_csv(recovered_url, country_or_region)
+
+    last_hundred_days_confirmed = confirmed_data.iloc[:, -101:].values[0]
+    last_hundred_days_new_confirmed = []
+    for i in range(1, len(last_hundred_days_confirmed)):
+        new_confirmed = last_hundred_days_confirmed[i] - \
+            last_hundred_days_confirmed[i - 1]
+        last_hundred_days_new_confirmed.append(new_confirmed)
+
+    # , deaths_data, recovered_data
+    return np.array(last_hundred_days_new_confirmed)
 
 
 def polt_scatter(train, prediction):
     fig = make_subplots(rows=3, cols=1)
     fig.add_trace(
         plotly.graph_objs.Scatter(x=np.arange(100), mode='lines', y=train, marker=dict(color="dodgerblue"),
-                name="Train"), row=1, col=1
+                                  name="Train"), row=1, col=1
     )
     fig.add_trace(
         plotly.graph_objs.Scatter(x=np.arange(100, 130), y=prediction, mode='lines', marker=dict(color="darkorange"),
-                name="Val"), row=1, col=1
+                                  name="Val"), row=1, col=1
     )
-    fig.update_layout(height=2000, width=1600, title_text="Exponential smoothing")
+    fig.update_layout(height=2000, width=1600,
+                      title_text="Exponential smoothing")
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
